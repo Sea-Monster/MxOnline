c8b7c28d7997e2059317eac3aab0d2d03516088d
Sea-Monster <cristianofigo@126.com>
2017-10-06 11:03:07 +0800

    修改性别字段大小

--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name='昵称', default='')
     birday = models.DateField(verbose_name='生日', null=True, blank=True)
-    gender = models.CharField(choices=(('male','男'),('female','女')),default='female', max_length=5)
+    gender = models.CharField(choices=(('male','男'),('female','女')),default='female', max_length=6)
     address = models.CharField(max_length=100, default='', verbose_name='地址')
     mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name='手机号码')
     image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png', max_length=100)
